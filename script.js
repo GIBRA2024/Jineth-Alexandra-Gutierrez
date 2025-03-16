2bb10f375eda2f8ed503cc810627214729d7921d
Mateo Monroy <monroymateo1@gmail.com>
2025-03-16 12:32:33 -0500

    listo

---
+++ script.js
@@ -0,0 +1,18 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+N:Gutierrez;Jineth;Alexandra;;
+FN:Jineth Alexandra Gutierrez
+TEL:+57 3118810870
+EMAIL;TYPE=WORK: ventasmedellín@cervalle.com 
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
